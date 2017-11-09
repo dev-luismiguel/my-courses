82c93e775821400965ac3cfad6200a65f84d51a1
Luís Miguel Abrantes <lmigueldev@gmail.com>
2017-11-09 18:02:20 -0300

    initial commit

---
+++ udemy--nodejs/curso-completo-do-desenvolvedor-nodejs/app/routes/noticias.js
@@ -0,0 +1,8 @@
+module.exports = function(app) {    
+    app.get('/noticias', function(req,res) {
+        var connection = app.config.dbConnection();
+        connection.query('SELECT * FROM news', function(error, result) {
+            res.render("noticias/noticias", { noticias : result });
+        });
+    });    
+};
