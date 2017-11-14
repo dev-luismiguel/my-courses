5933fe51b7e106e8867e5b35c74658a24c2dcc15
Luís Miguel Abrantes <lmigueldev@gmail.com>
2017-11-13 23:44:16 -0300

    added controllers

---
+++ udemy--nodejs/curso-completo-do-desenvolvedor-nodejs/app/controllers/admin.js
@@ -0,0 +1,27 @@
+module.exports.formulario_inclusao_noticia = function(application, req, res) {
+    res.render('admin/form_add_noticia', { validator : {}, noticia : {}} );    
+}
+
+module.exports.noticias_salvar = function(application, req, res) {
+    var noticia = req.body;
+    
+    req.assert('title', 'Título é obrigatório').notEmpty();
+    req.assert('summary', 'Resumo é obrigatório').notEmpty();
+    req.assert('summary', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+    req.assert('autor', 'Autor é obrigatório').notEmpty();
+    req.assert('news_date', 'Data é obrigatório').notEmpty();
+    req.assert('news', 'Notícia é obrigatório').notEmpty();
+    
+    var errors = req.validationErrors();
+    
+    if (errors) {
+        res.render("admin/form_add_noticia", { validator : errors, noticia : noticia});
+        return;
+    }
+    
+    var connection = application.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+    noticiasModel.salvarNoticia(noticia, function(error, result) {
+        res.redirect('/noticias');
+    });
+}
--- udemy--nodejs/curso-completo-do-desenvolvedor-nodejs/app/routes/admin.js
+++ udemy--nodejs/curso-completo-do-desenvolvedor-nodejs/app/routes/admin.js
@@ -1,29 +1,9 @@
 module.exports = function(app) {
     app.get('/formulario_inclusao_noticia', function(req, res) {
-        res.render('admin/form_add_noticia', { validator : {}} );
+        app.app.controllers.admin.formulario_inclusao_noticia(app, req, res);
     });
 
     app.post('/noticias/salvar', function(req, res) {
-        var noticia = req.body;
-
-        req.assert('title', 'Título é obrigatório').notEmpty();
-        req.assert('summary', 'Resumo é obrigatório').notEmpty();
-        req.assert('summary', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
-        req.assert('autor', 'Autor é obrigatório').notEmpty();
-        req.assert('news_date', 'Data é obrigatório').notEmpty();
-        req.assert('news', 'Notícia é obrigatório').notEmpty();
-
-        var errors = req.validationErrors();
-
-        if (errors) {
-            res.render("admin/form_add_noticia", { validator : errors });
-            return;
-        }
-
-        var connection = app.config.dbConnection();
-        var noticiasModel = new app.app.models.NoticiasDAO(connection);
-        noticiasModel.salvarNoticia(noticia, function(error, result) {
-            res.redirect('/noticias');
-        })
+        app.app.controllers.admin.noticias_salvar(app, req, res);        
     });
 }
